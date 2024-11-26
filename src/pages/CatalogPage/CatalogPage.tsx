05b3c01aafa7154815de2958f1a27f1ba36aa1bd
Yurii Kryvorotko <krivorotkokrivorotko2016@gmail.com>
2024-11-26 17:33:14 +0200

    start to filter

--- src/pages/CatalogPage/CatalogPage.tsx
+++ src/pages/CatalogPage/CatalogPage.tsx
@@ -1,36 +1,11 @@
-// import { useEffect, useState } from 'react';
 import { Loader } from '../../components/Loader';
 import { fetchRecipesByLetters } from '../../utils/fetchAllRecipes';
 import styles from './CatalogPage.module.scss';
-// import { Recepy } from '../../types/recipy';
 import { RecepyList } from '../../components/RecepyList/RecepyList';
 import { useQuery } from 'react-query';
 
 export const CatalogPage = () => {
   const { data, isLoading } = useQuery('recepies', fetchRecipesByLetters);
-
-  // const [isLoading, setIsLoading] = useState(false);
-  // const [recepies, setRecepies] = useState<Recepy[]>([]);
-
-  // const handleLoadProducts = () => {
-  //   setIsLoading(true);
-
-  //   fetchRecipesByLetters()
-  //     .then(data => {
-  //       setRecepies(data);
-  //     })
-  //     .catch(() => {
-  //       throw new Error('a');
-  //     })
-  //     .finally(() => {
-  //       setIsLoading(false);
-  //     });
-  // };
-
-  // useEffect(() => {
-  //   handleLoadProducts();
-  //   // eslint-disable-next-line react-hooks/exhaustive-deps
-  // }, []);
 
   return (
     <div>
